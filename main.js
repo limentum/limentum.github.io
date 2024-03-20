f009f45b82203aad9824cb21d2a72f81ad1e93df
Owen Farmer <owenfarmer.456@gmail.com>
2024-03-19 21:01:20 -0600

    Disable right-click and dragging on logo because idk

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-document.getElementById('logo').addEventListener('contextmenu', (e) => { // Disable right-click on logo
-    e.preventDefault(); // Prevent the default context menu
-  });
+window.onload = function() {
+	var logo = document.getElementById('logo');
+	logo.addEventListener('contextmenu', function(e) { // Disable right-click on logo
+		e.preventDefault(); // Prevent the default context menu
+	});
+	logo.addEventListener('dragstart', function(e) { // Disable dragging of the logo
+		e.preventDefault(); // Prevent the default drag action
+	});
+};
