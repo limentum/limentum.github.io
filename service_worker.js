0d7c5cba71118fd7afc79b9b08a2402a9caf51a6
Owen Farmer <owenfarmer.456@gmail.com>
2024-04-09 19:29:16 -0600

    Add service worker for offline caching

---
+++ service_worker.js
@@ -0,0 +1,63 @@
+// Define the name of the current cache
+var APP_PREFIX = 'LmntnPrjcts_';
+var VERSION = 'v_04';
+var SITE_CACHE = APP_PREFIX + VERSION;
+var URLS = [
+  '/index.html',
+  '/assets/css/style.css',
+  '/assets/css/chapter.css',
+  '/vestige/index.html',
+  '/assets/images/logo512.png',
+  '/assets/images/favicon.ico',
+  '/assets/js/async.js',
+  '/assets/js/script.js',
+];
+
+fetch('/vestige.json')
+.then(response => response.json())
+.then(data => {
+  URLS = URLS.concat(data);
+});
+
+// The install event
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(SITE_CACHE)
+      .then(function(cache) {
+        return cache.addAll(URLS);
+      })
+  );
+});
+
+// The fetch event
+self.addEventListener('fetch', function(event) { // Fetch event
+  event.respondWith( // Respond with
+    caches.open(SITE_CACHE).then(function(cache) { // Open the cache
+      return cache.match(event.request).then(function(response) { // Match the request with the cache content
+        if (response) {
+          return response; // Return the cached response
+        }
+        var fetchPromise = fetch(event.request).then(function(networkResponse) { // Fetch the request from the network and store it in a variable to cache
+          cache.put(event.request, networkResponse.clone()); // Put the request in the cache
+          return networkResponse; // Return the network response
+        })
+        return fetchPromise; // Return the network response if there's no cached response
+      })
+    })
+  );
+});
+
+// The activate event
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName.startsWith(APP_PREFIX) && cacheName != SITE_CACHE) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
