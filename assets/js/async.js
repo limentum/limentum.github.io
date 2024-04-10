808c7e75ff68f9cc246bd577fa3be7c38ba7765a
Owen Farmer <owenfarmer.456@gmail.com>
2024-04-10 10:56:34 -0600

    Refactor async.js to only show onscroll event once

--- assets/js/async.js
+++ assets/js/async.js
@@ -1,37 +1,35 @@
 let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', function(event) {
-  event.preventDefault(); // Prevent the default browser install prompt
-  deferredPrompt = event; // Store the event for later use
+window.addEventListener('beforeinstallprompt', (event) => { // listen for beforeinstallprompt event
+  event.preventDefault(); // prevent default prompt
+  deferredPrompt = event; // store the event for later use
 });
 
 let hasScrolled = false;
 let promptShown = false;
 
-window.onscroll = function() {
-  hasScrolled = true;
+window.onscroll = () => { // wait for user to scroll before showing prompt
+  if (!hasScrolled) { // only show prompt if user has scrolled
+    hasScrolled = true; // only show prompt once
+    if (!promptShown) { // only show prompt once
+      setTimeout(showInstallPrompt, 15000); // show prompt after 15 seconds
+    } 
+  }
 };
 
-setTimeout(function() {
-  if (hasScrolled && !promptShown) {
-    showInstallPrompt();
-    promptShown = true;
-  }
-}, 15000);
-
-// Call this function when you want to show the prompt, for example, when a button is clicked
-async function showInstallPrompt() {
-  if (!deferredPrompt) return;
-  deferredPrompt.prompt(); // Show the install prompt
+async function showInstallPrompt() { // show install prompt
+  if (!deferredPrompt) return; // do nothing if prompt is not available
+  deferredPrompt.prompt();// show prompt
   try {
-    const choiceResult = await deferredPrompt.userChoice;
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the install prompt');
-    } else {
-      console.log('User dismissed the install prompt');
+    const choiceResult = await deferredPrompt.userChoice; // wait for user to respond to prompt
+    if (choiceResult.outcome === 'accepted') { // user accepted the prompt
+      console.log('User accepted the install prompt'); // log the result
+    } else { // user dismissed the prompt
+      console.log('User dismissed the install prompt'); // log the result
     }
-    deferredPrompt = null;
+    deferredPrompt = null; // reset the deferred prompt
+    promptShown = true; // only show prompt once
   } catch (err) {
-    console.log('Error with install prompt: ', err);
+    console.log('Error with install prompt: ', err); // log any errors
   }
 }
